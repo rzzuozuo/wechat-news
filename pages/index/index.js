2f4ea5a39f4b33754920fc938f035e7f82588cdb
王成龙——PC <326727955@qq.com>
2019-07-23 15:01:01 +0800

    备份 准备使用模板

--- pages/index/index.js
+++ pages/index/index.js
@@ -22,35 +22,73 @@ Page({
         type: 'ty'
       }, {
         id: '其他',
-        type: 'qt'
+        type: 'other'
       }
     ],
-    newsArray: []
+    gnnewsArray: [],
+    gjnewsArray: [],
+    cjnewsArray: [],
+    ylnewsArray: [],
+    jsnewsArray: [],
+    tynewsArray: [],
+    othernewsArray: [],
+    newsArray: [],
+    height: "2000rpx",
+    currentNewsTypeIndex: 0
   },
   onLoad() {
-    this.getNews()
+    this.getNews(0)
   },
-  onPullDownRefresh() {
-    this.getNews(() => {
-      wx.stopPullDownRefresh()
+  onReachBottom() {
+    this.getNews(this.data.currentNewsTypeIndex)
+  },
+  contentSwiperFinish: function(event) {
+    console.log("animationfinish");
+    this.setData({
+      currentNewsTypeIndex: event.detail.current
     })
+    console.log("currentNewArrayLenght", this.data.newsArray[this.data.currentNewsTypeIndex].length)
+    this.getNews(event.detail.current);
   },
-  getNews() {
+  getNews(index, callback) {
+    console.log("getNews index:", index)
+    console.log("request new type:", this.data.newstype[index].type)
     wx.request({
-      url: 'https://test-miniprogram.com/api/news/list', //仅为示例，并非真实的接口地址
-      data: {
-        type: "gn"
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        this.data.newsArray = res.data.result.concat(this.data.newsArray)
+        url: 'https://test-miniprogram.com/api/news/list', 
+        data: {
+          type: this.data.newstype[index].type
+        },
+        header: {
+          'content-type': 'application/json' // 默认值
+        },
+        success: res => {
+
+          let array = res.data.result
+          console.log("old array:", array)
+          //处理字符串长度，防止超出范围
+          for (var i = 0; i < array.length; ++i) {
+            array[i].title = array[i].title.slice(0, 25) + "..."
+            array[i].date = array[i].date.slice(0, 10)
+          }
+          switch (this.data.newstype[index].type) {
+            case 'gn':
+              this.data.gnnewsArray = array
+              break;
+            case 'gj':
+              this.data.gjnewsArray = array
+              break;
+          }
+          
         this.setData({
-          newsArray: this.data.newsArray
+          newsArray: array
+        })
+        this.data.height = 414 + 193 * this.data.newsArray.length - 2 + 100
+        this.setData({
+          height: this.data.height + 'rpx'
         })
         console.log(this.data.newsArray)
+        callback && callback()
       }
     })
-  }
+}
 })
