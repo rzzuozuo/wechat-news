d31e1654fb409b90d6a4b1cbfc30d22747f81502
xiaowu <326727955@qq.com>
2019-07-23 22:10:55 +0800

    修改了swiper 切换后获取新闻的时机

--- pages/index/index.js
+++ pages/index/index.js
@@ -47,9 +47,9 @@ Page({
         color: '#ffffff'
       }
     ],
-    height: '2000rpx',
-    currentNewsTypeIndex: 0,
-    current: 0
+    height: '0rpx',//当前页 swiper height
+    currentNewsTypeIndex: 0,//切换前的current
+    current: 0   //实时current
   },
   onLoad() {
     this.getNews(0)
@@ -93,21 +93,20 @@ Page({
     this.getNews(this.data.currentNewsTypeIndex)
   },
   swiperChange: function(event) {
-    console.log("swiper change:",event)
+    console.log("swiper change")
     if (event.detail.source != "autoplay") {
     this.data.newstype[this.data.currentNewsTypeIndex].color = '#ffffff'
     this.data.newstype[event.detail.current].color = '#f6cb76'
     this.setData({
       newstype: this.data.newstype,
+      currentNewsTypeIndex: event.detail.current
     })
+      this.getNews(event.detail.current);
   }
 },
 contentSwiperFinish: function(event) {
   console.log("Swiper finish");
-  this.setData({
-    currentNewsTypeIndex: event.detail.current
-  })
-  this.getNews(event.detail.current);
+  
 },
 navigatToContent: function(event) {
   wx.navigateTo({
@@ -126,7 +125,7 @@ getNews(index, callback) {
     success: res => {
       console.log("getNews success")
       let array = res.data.result
-      console.log(array)
+      console.log("getted array:",array)
       //处理字符串长度，防止超出范围
       for (var i = 0; i < array.length; ++i) {
         array[i].title = array[i].title.slice(0, 25) + "..."
@@ -135,7 +134,7 @@ getNews(index, callback) {
       this.data.newstype[index].news = array
 
 
-      this.data.newstype[index].height = 414 + 193 * this.data.newstype[index].news.length - 2 + 100 + "rpx"
+      this.data.newstype[index].height = 414 + 193 * this.data.newstype[index].news.length - 2 + 100
       this.data.height = this.data.newstype[index].height
       this.setData({
         newstype: this.data.newstype,
