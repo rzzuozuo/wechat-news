7354f89b8de104b5be8e9f4fe30de0520c2a37c7
xiaowu <326727955@qq.com>
2019-07-23 21:47:12 +0800

    添加了导航栏按钮

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,7 +6,7 @@ Page({
         type: "gn",
         news: [],
         height: '0',
-      color: '#f6cb76'
+        color: '#f6cb76'
       },
       {
         id: '国际',
@@ -48,59 +48,101 @@ Page({
       }
     ],
     height: '2000rpx',
-    currentNewsTypeIndex: 0
+    currentNewsTypeIndex: 0,
+    current: 0
   },
   onLoad() {
     this.getNews(0)
   },
+  navbtn_gn: function(event) {
+    this.setData({
+      current: 0
+    })
+  },
+  navbtn_gj: function(event) {
+    this.setData({
+      current: 1
+    })
+  },
+  navbtn_cj: function(event) {
+    this.setData({
+      current: 2
+    })
+  },
+  navbtn_yl: function(event) {
+    this.setData({
+      current: 3
+    })
+  },
+  navbtn_js: function(event) {
+    this.setData({
+      current: 4
+    })
+  },
+  navbtn_ty: function(event) {
+    this.setData({
+      current: 5
+    })
+  },
+  navbtn_other: function(event) {
+    this.setData({
+      current: 6
+    })
+  },
   onReachBottom() {
     this.getNews(this.data.currentNewsTypeIndex)
-
   },
-  contentSwiperFinish: function(event) {
-    console.log("Swiper finish");
+  swiperChange: function(event) {
+    console.log("swiper change:",event)
+    if (event.detail.source != "autoplay") {
     this.data.newstype[this.data.currentNewsTypeIndex].color = '#ffffff'
     this.data.newstype[event.detail.current].color = '#f6cb76'
     this.setData({
-      newstype:this.data.newstype,
-      currentNewsTypeIndex: event.detail.current
-    })
-    this.getNews(event.detail.current);
-  },
-  navigatToContent: function(event){
-    wx.navigateTo({
-      url: '../../pages/news/new'
-    })
-  },
-  getNews(index, callback) {
-    wx.request({
-      url: 'https://test-miniprogram.com/api/news/list',
-      data: {
-        type: this.data.newstype[index].type
-      },
-      header: {
-        'content-type': 'application/json' // 默认值
-      },
-      success: res => {
-        console.log("getNews success")
-        let array = res.data.result
-        console.log(array)
-        //处理字符串长度，防止超出范围
-        for (var i = 0; i < array.length; ++i) {
-          array[i].title = array[i].title.slice(0, 25) + "..."
-          array[i].date = array[i].date.slice(0, 10)
-        }
-        this.data.newstype[index].news = array
-
-
-        this.data.newstype[index].height = 414 + 193 * this.data.newstype[index].news.length - 2 + 100 + "rpx"
-        this.data.height = this.data.newstype[index].height
-        this.setData({
-          newstype: this.data.newstype,
-          height: this.data.height
-        })
-        callback && callback()
-      }
+      newstype: this.data.newstype,
     })
   }
+},
+contentSwiperFinish: function(event) {
+  console.log("Swiper finish");
+  this.setData({
+    currentNewsTypeIndex: event.detail.current
+  })
+  this.getNews(event.detail.current);
+},
+navigatToContent: function(event) {
+  wx.navigateTo({
+    url: '../../pages/news/new'
+  })
+},
+getNews(index, callback) {
+  wx.request({
+    url: 'https://test-miniprogram.com/api/news/list',
+    data: {
+      type: this.data.newstype[index].type
+    },
+    header: {
+      'content-type': 'application/json' // 默认值
+    },
+    success: res => {
+      console.log("getNews success")
+      let array = res.data.result
+      console.log(array)
+      //处理字符串长度，防止超出范围
+      for (var i = 0; i < array.length; ++i) {
+        array[i].title = array[i].title.slice(0, 25) + "..."
+        array[i].date = array[i].date.slice(0, 10)
+      }
+      this.data.newstype[index].news = array
+
+
+      this.data.newstype[index].height = 414 + 193 * this.data.newstype[index].news.length - 2 + 100 + "rpx"
+      this.data.height = this.data.newstype[index].height
+      this.setData({
+        newstype: this.data.newstype,
+        height: this.data.height
+      })
+      callback && callback()
+    }
+  })
+}
 })
